28539a731f122bef619380878d1863ecde4650d5
Your Name <your.email@example.com>
2013-10-25 11:10:50 +0700

    Finish test pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TestApp::Application.routes.draw do
   get "test_pages/home"
   get "test_pages/help"
+  get "test_pages/about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
